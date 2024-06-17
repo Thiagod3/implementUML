77a74790a2d4713a25b67e063d3bb2bb089ae27c
Thiagod3 <thiago.vilela3@outlook.com>
2024-06-17 14:30:42 -0300

    projeto criado

---
+++ src/main/java/com/thiagoCompass/ImplementacaoUML/ImplementacaoUmlApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoCompass.ImplementacaoUML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementacaoUmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementacaoUmlApplication.class, args);
+	}
+
+}
