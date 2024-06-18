600ee26c3d53a6851444498ee02d6c9f76a705d4
Thiagod3 <thiago.vilela3@outlook.com>
2024-06-18 14:26:12 -0300

    produto e associação many to many

--- src/main/java/com/thiagoCompass/ImplementacaoUML/ImplementacaoUmlApplication.java
+++ src/main/java/com/thiagoCompass/ImplementacaoUML/ImplementacaoUmlApplication.java
@@ -1,6 +1,9 @@
 package com.thiagoCompass.ImplementacaoUML;
 
+
+
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,13 +11,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.thiagoCompass.ImplementacaoUML.domain.Categoria;
+import com.thiagoCompass.ImplementacaoUML.domain.Produto;
 import com.thiagoCompass.ImplementacaoUML.repositories.CategoriaRepository;
+import com.thiagoCompass.ImplementacaoUML.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ImplementacaoUmlApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriarepository;
+	
+	@Autowired
+	private ProdutoRepository produtorepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImplementacaoUmlApplication.class, args);
@@ -25,7 +33,21 @@ public class ImplementacaoUmlApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 200.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Computador", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriarepository.saveAll(Arrays.asList(cat1,cat2));
+		produtorepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 		
 	}
 
