3872b61fdc9c46ee36dc931fe9ae20b0719057cd
Thiagod3 <thiago.vilela3@outlook.com>
2024-06-18 13:43:15 -0300

    criando operação de instanciação

--- src/main/java/com/thiagoCompass/ImplementacaoUML/ImplementacaoUmlApplication.java
+++ src/main/java/com/thiagoCompass/ImplementacaoUML/ImplementacaoUmlApplication.java
@@ -1,13 +1,32 @@
 package com.thiagoCompass.ImplementacaoUML;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.thiagoCompass.ImplementacaoUML.domain.Categoria;
+import com.thiagoCompass.ImplementacaoUML.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ImplementacaoUmlApplication {
+public class ImplementacaoUmlApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriarepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImplementacaoUmlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriarepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
